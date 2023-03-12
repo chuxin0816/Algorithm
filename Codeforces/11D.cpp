26389328bf35d2d49a34171ad03ceac6e2044cb6
chuxin <2670957513@qq.com>
2023-03-12 20:31:59 +0800

    CF11D

--- Codeforces/11D.cpp
+++ Codeforces/11D.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll=long long ;
-const ll mod =998244353;
-int main(){
-    int n,m;
-    cin>>n>>m;
-
+#define lowbit(x) (x & -x)
+const int N = 1 << 20;
+long long f[N][20], n, m, t, ans;
+int main() {
+  cin >> n >> m;
+  t = 1 << n;
+  vector<int> adl[20];
+  for (int i = 0; i < m; ++i) {
+    int a, b;
+    cin >> a >> b;
+    adl[a].push_back(b);
+    adl[b].push_back(a);
+  }
+  for (int i = 1; i <= n; ++i) f[1 << (i - 1)][i] = 1;
+  for (int i = 1; i < t; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (!f[i][j]) continue;
+      for (auto k : adl[j]) {
+        int K = 1 << (k - 1);
+        if (K < lowbit(i)) continue;
+        if (K & i) {
+          if (K == lowbit(i)) ans += f[i][j];
+        } else
+          f[i | K][k] += f[i][j];
+      }
+    }
+  }
+  cout << (ans - m) / 2 << endl;
+  return 0;
 }
