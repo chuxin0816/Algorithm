a4f0289628de0d74220cb5d5a9f50fb3042adbda
chuxin <2670957513@qq.com>
2023-03-28 19:45:36 +0800

    renew

--- test.cpp
+++ test.cpp
@@ -1,25 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-class student{
-    private:
-    int x1,x2,x3;
-    int *s=new int[1000];
-    public:
-    student():x1(0),x2(0),x3(0){}
-    // student(int x1,int x2,int x3):x1(x1),x2(x2),x3(x3){}
-    /* student(int x1,int x2,int x3){
-        x1=x1;
-        x2=x2;
-        x3=x3;
-    } */
-    void print() {
-        cout<<x1<<' '<<x2<<' '<<x3<<endl;
-    }
-};
-student a(1,2,3);
 int main() {
-    
-    a.print();
-    return 0;
+  vector<int> a{1, 2, 3, 4, 5, 6, 7, 8};
+  sort(a.begin(), a.end(), [](int a, int b) { return a > b; });
+  for (auto e : a) cout << e << ' ';
+  cout << endl;
+  return 0;
 }
-
